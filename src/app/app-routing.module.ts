c25df69484a61541740cf24bbdf33ec4c80d54d9
Omar BARRO <barroomar@gmail.com>
2020-04-12 23:10:45 +0200

    fc

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductsComponent } from './products/products.component';
+import { AddProductsComponent } from './add-products/add-products.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "products",component: ProductsComponent
+  },
+  {
+    path: "addproducts",component: AddProductsComponent
+  },
+  {
+    path: "",redirectTo: "/products",pathMatch:"full"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { AddProductsComponent } from './add-products/add-products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    AddProductsComponent
   ],
   imports: [
     BrowserModule,
